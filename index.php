2357ea1971e53c7ce5013669d837178b0df8f07c
Mohammad Shimanto <78781611+flashkiss@users.noreply.github.com>
2023-07-24 23:01:12 +0600

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<body>
+<script>
+  
+  
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-66662547-1', 'auto');
+  ga('send', 'pageview');
+</script>
+
+<form action=# method="get">
+<input type="text" name="email"><input type="submit" value="Subscribe!">
+</form>
+
+Thank you for subscription!<br>
+Email <b></b> added to mailing list!
+
+</body>
+</html>
